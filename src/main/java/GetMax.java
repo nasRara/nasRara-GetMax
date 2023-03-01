a0a8ee58d907183d889b2e89c56abaa839f64209
nasRara <pastasaqajan@icloud.com>
2023-03-01 22:17:56 +0000

    max

--- src/main/java/GetMax.java
+++ src/main/java/GetMax.java
@@ -7,6 +7,12 @@ public class GetMax {
      * @return the largest value in arr.
      */
     public int max(int[] arr){
-        return -1;
+        int biggest= -1000;
+        for (int i=0; i<arr.length; i++){
+            if(biggest< arr[i]){
+                biggest= arr[i];
+            }
+        }
+        return biggest;
     }
 }
